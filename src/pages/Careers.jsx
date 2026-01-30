ba92c773a49d76f0bb0a52fb75d38e30da51acf9
sahil-ApplauseITSolutions <sahil@applauseitsolutions.com>
2026-01-30 17:30:19 +0530

    add

--- src/pages/Careers.jsx
+++ src/pages/Careers.jsx
@@ -313,11 +313,15 @@ const Careers = () => {
                       <div className="font-bold">FOR MORE DETAILS</div>
                       <div className="flex items-center justify-center gap-4">
                         <div className="flex items-center">
-                          <span className="text-white mr-2">🌐</span>
+                          <svg className="w-5 h-5 text-white mr-2" fill="currentColor" viewBox="0 0 20 20">
+                            <path fillRule="evenodd" d="M4.083 9h1.946c.089-1.546.383-2.97.837-4.118A6.004 6.004 0 004.083 9zM10 2a8 8 0 100 16 8 8 0 000-16zm0 2c-.076 0-.232.032-.465.262-.238.234-.497.623-.737 1.182-.389.907-.673 2.142-.766 3.556h3.936c-.093-1.414-.377-2.649-.766-3.556-.24-.56-.5-.948-.737-1.182C10.232 4.032 10.076 4 10 4zm3.971 5c-.089-1.546-.383-2.97-.837-4.118A6.004 6.004 0 0115.917 9h-1.946zm-2.003 2H8.032c.093 1.414.377 2.649.766 3.556.24.56.5.948.737 1.182.233.23.389.262.465.262.076 0 .232-.032.465-.262.238-.234.498-.623.737-1.182.389-.907.673-2.142.766-3.556zm1.166 4.118c.454-1.147.748-2.572.837-4.118h1.946a6.004 6.004 0 01-2.783 4.118zm-6.268 0C6.412 13.97 6.118 12.546 6.03 11H4.083a6.004 6.004 0 002.783 4.118z" clipRule="evenodd" />
+                          </svg>
                           <span>www.aspireks.com/internship</span>
                         </div>
                         <div className="flex items-center">
-                          <span className="text-white mr-2">📞</span>
+                          <svg className="w-5 h-5 text-white mr-2" fill="currentColor" viewBox="0 0 20 20">
+                            <path d="M2 3a1 1 0 011-1h2.153a1 1 0 01.986.836l.74 4.435a1 1 0 01-.54 1.06l-1.548.773a11.037 11.037 0 006.105 6.105l.774-1.548a1 1 0 011.059-.54l4.435.74a1 1 0 01.836.986V17a1 1 0 01-1 1h-2C7.82 18 2 12.18 2 5V3z" />
+                          </svg>
                           <span>020-25530291</span>
                         </div>
                       </div>
@@ -328,7 +332,9 @@ const Careers = () => {
                   <div className="bg-[#3D1717] text-white p-6 rounded-lg text-center">
                     <div className="space-y-2">
                       <div className="flex items-center justify-center">
-                        <span className="text-white text-2xl mr-3">🎓</span>
+                        <svg className="w-6 h-6 text-white mr-3" fill="currentColor" viewBox="0 0 20 20">
+                          <path fillRule="evenodd" d="M3 4a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zm0 4a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zm0 4a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zm0 4a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clipRule="evenodd" />
+                        </svg>
                         <span className="font-bold">Internship Enrollment fees 1500/- Per Student</span>
                       </div>
                       <div className="text-sm opacity-90">
